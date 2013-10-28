864379197cadcf73d48ebba155d35ef9833a8d11
Felix <felix.reidl@gmail.com>
2013-10-28 21:28:20 +0100

    Dirty commit. Added comments to SpriteSheetFactorty and fixed formatting in GameTest.

--- src/main/GameTest.java
+++ src/main/GameTest.java
@@ -18,83 +18,82 @@ import lufti.ui.SimpleWindow;
  */
 public class GameTest {
 
-    @SuppressWarnings("ResultOfObjectAllocationIgnored")
-    public static void main(String args[]) throws IOException {
-	SpriteSheet spr = SpriteSheetFactory.getClassic();
-	
+	@SuppressWarnings("ResultOfObjectAllocationIgnored")
+	public static void main(String args[]) throws IOException {
+		SpriteSheet spr = SpriteSheetFactory.getClassic();
 
-	SimpleGame.create(800, 600, spr);
-    }
-
-    static class SimpleGame extends AbstractGame {
-
-	private ArrayList<Point> bullets = new ArrayList<>();
-	private SpriteSheet sprites;
-	private int x, y, width, height;
-	private int ticker;
-	boolean rendering = false;
-
-	public SimpleGame(int width, int height, SpriteSheet spr) {
-	    x = width / 2;
-	    y = height - 75;
-	    this.width = width;
-	    this.height = height;
-	    ticker = 0;
-	    sprites = spr;
+		SimpleGame.create(800, 600, spr);
 	}
 
-	public static SimpleGame create(int width, int height, SpriteSheet spr) {
-	    SimpleGame res = new SimpleGame(width, height, spr);
+	static class SimpleGame extends AbstractGame {
 
-	    SimpleWindow window = SimpleWindow.create(width, height, 60, Color.BLACK);
-	    AbstractGame.attach(res, window, 60);
+		private ArrayList<Point> bullets = new ArrayList<>();
+		private SpriteSheet sprites;
+		private int x, y, width, height;
+		private int ticker;
+		boolean rendering = false;
 
-	    return res;
-	}
-
-	@Override
-	public void update(PlayerInput input) {
-	    assert (!rendering);
-
-	    if (input.containsCommand(PlayerInput.Command.SHOOT)) {
-		bullets.add(new Point(x - 2, y - 8));
-	    }
-
-	    if (input.containsCommand(PlayerInput.Command.LEFT)) {
-		x -= 4;
-		if (x < 50) {
-		    x = 50;
+		public SimpleGame(int width, int height, SpriteSheet spr) {
+			x = width / 2;
+			y = height - 75;
+			this.width = width;
+			this.height = height;
+			ticker = 0;
+			sprites = spr;
 		}
-	    }
 
-	    if (input.containsCommand(PlayerInput.Command.RIGHT)) {
-		x += 4;
-		if (x > width - 50) {
-		    x = width - 50;
+		public static SimpleGame create(int width, int height, SpriteSheet spr) {
+			SimpleGame res = new SimpleGame(width, height, spr);
+
+			SimpleWindow window = SimpleWindow.create(width, height, 60, Color.BLACK);
+			AbstractGame.attach(res, window, 60);
+
+			return res;
 		}
-	    }
 
-	    ListIterator<Point> listIterator = bullets.listIterator();
-	    while (listIterator.hasNext()) {
-		Point bullet = listIterator.next();
-		bullet.y -= 5;
-		if (bullet.y < -50) {
-		    listIterator.remove();
+		@Override
+		public void update(PlayerInput input) {
+			assert (!rendering);
+
+			if (input.containsCommand(PlayerInput.Command.SHOOT)) {
+				bullets.add(new Point(x - 2, y - 8));
+			}
+
+			if (input.containsCommand(PlayerInput.Command.LEFT)) {
+				x -= 4;
+				if (x < 50) {
+					x = 50;
+				}
+			}
+
+			if (input.containsCommand(PlayerInput.Command.RIGHT)) {
+				x += 4;
+				if (x > width - 50) {
+					x = width - 50;
+				}
+			}
+
+			ListIterator<Point> listIterator = bullets.listIterator();
+			while (listIterator.hasNext()) {
+				Point bullet = listIterator.next();
+				bullet.y -= 5;
+				if (bullet.y < -50) {
+					listIterator.remove();
+				}
+			}
+
+			ticker++;
 		}
-	    }
 
-	    ticker++;
+		@Override
+		public void render(Canvas.CanvasPainter pntr) {
+			rendering = true;
+			pntr.drawImage(sprites.getSprite("Ship", 0), x - 26, y - 10);
+
+			for (Point bullet : bullets) {
+				pntr.drawImage(sprites.getSprite("ProjectileA", 0), bullet.x, bullet.y);
+			}
+			rendering = false;
+		}
 	}
-
-	@Override
-	public void render(Canvas.CanvasPainter pntr) {
-	    rendering = true;
-	    pntr.drawImage(sprites.getSprite("Ship", 0), x - 26, y - 10);
-
-	    for (Point bullet : bullets) {
-		pntr.drawImage(sprites.getSprite("ProjectileA", 0), bullet.x, bullet.y);
-	    }
-	    rendering = false;
-	}
-    }
 }
